ed49fe28f9840a6d989fa1cc35f6c7a2010731ff
SamudraW <152139583+SamudraW@users.noreply.github.com>
2026-05-02 02:28:32 +0530

    done

--- Frontend/src/Services/MenuService.js
+++ Frontend/src/Services/MenuService.js
@@ -6,12 +6,12 @@ const api = axios.create({
 });
 
 const MenuService = {
-  getAll: () => api.get('/menu'),
-  getById: (id) => api.get(`/menu/${id}`),
-  getByResId: (id) => api.get(`/menu/getbyresid/${id}`),
-  create: (data) => api.post('/menu', data),
-  update: (id, data) => api.put(`/menu/${id}`, data),
-  delete: (id) => api.delete(`/menu/${id}`),
+  getAll: () => api.get('/restaurant/menu'),
+  getById: (id) => api.get(`/restaurant/menu/${id}`),
+  getByResId: (id) => api.get(`/restaurant/menu/getbyresid/${id}`),
+  create: (data) => api.post('/restaurant/menu', data),
+  update: (id, data) => api.put(`/restaurant/menu/${id}`, data),
+  delete: (id) => api.delete(`/restaurant/menu/${id}`),
 };
 
 export default MenuService;
